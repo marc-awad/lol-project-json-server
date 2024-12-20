51180cef3527725b5f9199cfa6de87fbc4482312
Marc AWAD <marc.awad@supdevinci-edu.fr>
2024-12-20 14:57:53 +0100

    Adding deleting and adding on the json db file functions

--- public/script.js
+++ public/script.js
@@ -1,6 +1,63 @@
 const divChamp = document.getElementById("divChamp")
 const champions = document.getElementById("allChamp")
+const addButton = document.getElementById("addButton")
+const deleteButton = document.getElementById("deleteButton")
 
+//Cacher la div si aucun champion
 if (champions.innerHTML == "") {
   divChamp.hidden = true
+} else {
+  divChamp.hidden = false
 }
+
+addButton.addEventListener("click", (e) => {
+  e.preventDefault()
+  let name = document.getElementById("name").value
+  let lane = document.getElementById("lane").value
+  let type = document.getElementById("type").value
+  let imageurl = document.getElementById("imageurl").value
+
+  addChampion(name, lane, type, imageurl)
+})
+
+function addChampion(name, lane, type, imageurl) {
+  let newChamp = {
+    name: name,
+    lane: lane,
+    type: type,
+    imageurl: imageurl,
+  }
+  fetch("http://localhost:3000/champions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newChamp),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Champion ajouté : ", data)
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'ajout du champion : ", error)
+    })
+}
+
+function deleteAllChampion() {
+  fetch("http://localhost:3000/champions")
+    .then((response) => response.json())
+    .then((champs) => {
+      champs.forEach((champ) => {
+        fetch(`http://localhost:3000/champions/${champ.id}`, {
+          method: "DELETE",
+        })
+      })
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la suppresion des champions : ", error)
+    })
+}
+
+deleteButton.addEventListener("click", () => {
+  deleteAllChampion()
+})
